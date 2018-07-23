028aa5b01fd0df867927ee6347a555d3e3e88006
pradeep singh <pradeep@saralweb.com>
2018-07-23 11:20:12 +0530

    face detection test code checked in

--- basic-operations/rotate.py
+++ basic-operations/rotate.py
@@ -1,15 +1,3 @@
-# Copyright 2017 BIG VISION LLC ALL RIGHTS RESERVED
-# 
-# This code is made available to the students of 
-# the online course titled "OpenCV for Beginners" 
-# by Satya Mallick for personal non-commercial use. 
-#
-# Sharing this code is strictly prohibited without written
-# permission from Big Vision LLC. 
-#
-# For licensing and other inquiries, please email 
-# spmallick@bigvisionllc.com 
-#
 
 import cv2
 
